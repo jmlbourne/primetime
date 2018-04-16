a4e9fb82a010c0dd53b4d1cf8e2dcb3e6f7bb46a
Matt Bourne <matt@qdrngl.com>
2018-04-16 19:44:49 +1000

    Added first version

---
+++ primetime.py
@@ -0,0 +1,15 @@
+import math
+starting_num = 2
+finishing_num = 100
+i = starting_num
+
+while i <= finishing_num:
+    j = 2
+    prime_flag = True
+    while j <= math.sqrt(i):
+        if i % j == 0:
+            prime_flag = False           
+        j += 1
+    if prime_flag == True:
+        print(i, "is a prime number")
+    i += 1
